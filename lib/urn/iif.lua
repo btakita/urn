cb0c258e38ef7fed19bbe68238a745402dfb7af7
Brian Takita <brian.takita@gmail.com>
2008-09-14 00:02:28 -0700

    Removing existing sample wx lua Editor. Going to start from scratch using the Editor as a reference.

--- lib/urn/iif.lua
+++
@@ -1,2 +0,0 @@
--- Equivalent to C's "cond ? a : b", all terms will be evaluated
-function iff(cond, a, b) if cond then return a else return b end end
--- lib/urn/is_lua_file.lua
+++
@@ -1,4 +0,0 @@
-function IsLuaFile(filePath)
-    return filePath and (string.len(filePath) > 4) and
-           (string.lower(string.sub(filePath, -4)) == ".lua")
-end
